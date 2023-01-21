ad8451658a00052edcdb7f5f4cde4aee48396781
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2023-01-21 13:51:04 -0300

    commit

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,19 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+
 import { appRoutes } from "./routes"
 
 const app = Fastify()
 
-app.register(cors)
+app.register(cors,  {
+  origin: true,
+})
+
 app.register(appRoutes)
 
 app.listen({
   port: 3333,
-}).then(() => {
-  console.log('HTTP Server running!')
+  host: '0.0.0.0',
+}).then((url) => {
+  console.log(`HTTP Server running on ${url}`)
 })
